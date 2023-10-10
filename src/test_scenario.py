a96c3b8eb98a794714258b07f05cdeff903acb58
tirafrutas <smoralesg@uoc.edu>
2023-10-10 16:35:12 +0200

    Sample list of prompts by TestScenario.num_tests

--- src/test_scenario.py
+++ src/test_scenario.py
@@ -1,4 +1,5 @@
 from prompt import Prompt
+from random import sample
 
 class TestScenario:
 
@@ -8,16 +9,17 @@ class TestScenario:
     
     @prompts.setter
     def prompts(self, value: [Prompt]):
-        self.set_prompts(value)
-        self.instantiate_prompts()
+        self.__select_prompts(value)
+        self.__instantiate_prompts()
 
     def __init__(self, cfg):
         self._timestamp = cfg['timestamp']
         self._temperature = cfg['temperature']
         self._tokens = cfg['tokens']
+        self._num_tests = cfg['numTests']
         self._ethical_requirements = cfg['requirements']
     
-    def set_prompts(self, prompts: [Prompt]):
+    def __select_prompts(self, prompts: [Prompt]):
         result = []
         for req in self._ethical_requirements:
             concern = req['concern']
@@ -25,12 +27,13 @@ class TestScenario:
             assessment_types = [assessment_type.lower() for assessment_type in req['assessments']]
             filtered = [prompt for prompt in prompts if prompt.concern == concern and prompt.type in prompt_types and prompt.assessment in assessment_types]
             result = result + filtered
-        self._prompts = result
+        self._prompts = sample(result, self._num_tests)
     
-    def instantiate_prompts(self):
+    def __instantiate_prompts(self):
         for req in self._ethical_requirements:
             concern = req['concern']
             communities = req['communities']
+            prompt: Prompt
             for prompt in self._prompts:
                 if (prompt.concern == concern):
                     prompt.instantiate(concern, communities)
