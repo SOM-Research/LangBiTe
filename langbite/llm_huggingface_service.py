93ca1bf89f9c69e9a1c73fb18ffdd3db4f44450c
mgv99 <gomezmarcos99@gmail.com>
2024-01-25 16:05:48 +0100

    Fixed huggingface API response format

--- langbite/llm_huggingface_service.py
+++ langbite/llm_huggingface_service.py
@@ -17,8 +17,7 @@ class HuggingFaceService(LLMService):
         self.model = model
     
     def query(self, payload):
-        data = json.dumps(payload)
-        response = requests.request('POST', self.model, headers=self.headers, data=data)
+        response = requests.post(self.model, headers=self.headers, json=payload)
         output = response.json()
         #return json.loads(response.content.decode("utf-8"))[0]['generated_text'] <- old GPT2 ones
         if 'error' in output: raise Exception('ERROR: ' + output['error'])
@@ -26,13 +25,14 @@ class HuggingFaceService(LLMService):
 
 class HuggingFaceCompletionService(HuggingFaceService):
     def execute_prompt(self, prompt):
-        output = self.query(prompt)
+        payload = {"inputs": prompt, "parameters": {"return_full_text": False, "temperature": self.temperature}}
+        output = self.query(payload)
         #data = json.dumps(prompt)
         #response = requests.request('POST', self.model, headers=self.headers, data=data)
         #output = response.json()
         #return json.loads(response.content.decode("utf-8"))[0]['generated_text'] <- old GPT2 ones
         if 'error' in output: raise Exception('ERROR: ' + output['error'])
-        return output['generated_text']
+        return output[0]['generated_text']
 
 class HuggingFaceQuestionAnsweringService(HuggingFaceService):
 
