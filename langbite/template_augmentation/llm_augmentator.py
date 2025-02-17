9469523f8b298fe9500cb55009215940d6dd25c9
tirafrutas <smoralesg@uoc.edu>
2025-02-17 13:23:50 +0100

    Removed unnecesary variables.

--- langbite/template_augmentation/llm_augmentator.py
+++ langbite/template_augmentation/llm_augmentator.py
@@ -14,19 +14,15 @@ class Augmentator:
         self.__llm_service: OpenAIChatService = llm_factory.factory.create(self.AUGMENTATOR_MODEL, **config)
 
     def execute(self, concern, communities, context, scenarios, num_templates, fake_markup):
-        sensitive_communities = ', '.join(communities)
-        first_community = communities[0]
-        second_community = communities[1]
         augmentation_prompt_template = PromptIOManager.load_augmentation_prompt()
         augmentation_prompt = augmentation_prompt_template.format(
             concern=concern,
-            sensitive_communities=sensitive_communities,
+            sensitive_communities=', '.join(communities),
             num_templates=num_templates,
             fake_markup=fake_markup,
-            first_community=first_community,
-            second_community=second_community,
+            first_community=communities[0],
+            second_community=communities[1],
             context=context,
             scenarios=scenarios)
-
         result = json.loads(self.__llm_service.execute_prompt(augmentation_prompt))
         return result
